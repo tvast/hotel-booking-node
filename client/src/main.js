7948fde0d2429846337f8a16a78d1f8023fda310
theo <theophile.vast@amadeus.com>
2020-02-12 11:06:05 +0100

    add style to search hotel

--- client/src/main.js
+++ client/src/main.js
@@ -30,11 +30,21 @@ Vue.mixin({
       var getdurationtime = input.split("PT")
       // window.console.log(getTImeHour)
       return getdurationtime[0]
+    },
+
+  truncateString(str, num) {
+  // If the length of str is less than or equal to num
+  // just return str--don't truncate it.
+          if (str.length <= num) {
+            return str
+          }
+          // Return str truncated with '...' concatenated to the end of str.
+          return str.slice(0, num) + '...'
+        }
     }
 
     
-   }
-});
+   })
 new Vue({
   router,
   store,
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,10 +1,47 @@
 <template>
-  <div class="about">
-    <div v-for="(item,index) in $store.getters.flavor.data" :key="index">
-<div v-for="(foo,index) in item.hotel.media" :key="index"><img :src="foo.uri"></div>
-    <h1>{{item.hotel.type}}</h1>
-    <!-- {{item.hotel.description.text}} -->
-</div>
 
+
+<div class="md-layout md-gutter md-alignment-center">
+    <div v-for="(item,index) in $store.getters.flavor.data" :key="index" class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+    	<div >
+<md-card>
+      <md-card-media-cover md-solid>
+        <md-card-media md-ratio="1:1">
+         <img src="https://picsum.photos/200/300">
+        </md-card-media>
+
+        <md-card-area>
+          <md-card-header>
+            <span class="md-title">{{item.hotel.type}}</span>
+            <span class="md-subhead">{{truncateString(item.hotel.description.text,124)}}</span>
+          </md-card-header>
+
+          <md-card-actions>
+            <md-button>available : <template v-if="item.available"><span id="dotsGreen">&#183;</span></template></md-button>
+            <md-button>BOOK</md-button>
+          </md-card-actions>
+        </md-card-area>
+      </md-card-media-cover>
+    </md-card>
+</div></div>
+   
   </div>
-</template>
+
+      </template>
+<style lang="scss" scoped>
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+    color: black
+  }
+
+  #dotsGreen{
+  	font-size: 45px;
+  	color:green;
+
+  }
+
+
+</style>
